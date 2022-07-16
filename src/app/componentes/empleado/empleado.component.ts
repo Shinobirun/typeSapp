beb538b2248bc3a994698b3b1885c79a2caf7751
Shinobirun <adrian.latorre@gmail.com>
2022-07-16 17:52:23 -0300

    bindingEvent

--- src/app/componentes/empleado/empleado.component.ts
+++ src/app/componentes/empleado/empleado.component.ts
@@ -1,3 +1,4 @@
+import { ParseTreeResult } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empleado.component.css']
 })
 export class EmpleadoComponent implements OnInit {
+ nombre="Adri";
+ apellido="Lato";
+ private  edad=50;
+ empresa="M & A";
+ frase="";
+ eBoton=false;
+ usuReg=false;
+ textReg="No esta registrado";
+
+ getEdad(){
+  return this.edad;
+ }
+
+llamaEmpresa(value:String) {}
+ GetRegUsu(){
+  this.usuReg=true;
+ }
+
+ setusuRegistrado(event:Event){
+ // alert("El usuario se acaba de registrar")
+ this.textReg="El usuario se acaba de Registrar "
+if((<HTMLInputElement>event.target).value=="si"){
+  this.textReg="El usuario se acaba de Registrar "
+}
+else{
+  this.textReg="El usuario no esta registrado "
+}
+
+ }
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
